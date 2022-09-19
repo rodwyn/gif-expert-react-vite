759770d8ccbbfe572e2ffc6a59fedb82850878c1
Rodwyn Josafat Moreno Ramirez <rodwyn.moreno@globant.com>
2022-09-19 09:33:15 -0500

    Added AddCategory component.

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] =useState(['Pikachu']);
+  const onAddCategory = newCategory => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <AddCategory
+          // setCategories={setCategories}
+          onNewCategory={ event => onAddCategory(event)}
+        />
+        
+        <ol>
+          {
+          categories.map( category => {
+            return <li key={category}>{category}</li>
+          })
+          }
+        </ol>
     </>
   )
 }
